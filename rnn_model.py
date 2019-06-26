38dd98561d42fd9b53a9cc2da85504636b4ed95f
Vaios Papaspyros <b.papaspyros@gmail.com>
2019-06-26 18:47:24 +0200

    Minor changes

--- rnn_model.py
+++ rnn_model.py
@@ -106,7 +106,7 @@ if __name__ == '__main__':
     # model.add(tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(y_train.shape[1], activation='tanh')))
 
     optimizer = tf.keras.optimizers.Adam(0.0001)
-    model.compile(loss='mse',
+    model.compile(loss='kullback_leibler_divergence',
                     optimizer=optimizer,
                     metrics=['mae'])
     model.summary()
