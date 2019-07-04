Fix variance issue in model

losses.py
@@ -10,7 +10,7 @@ def gaussian_nll_tanh(y_true, y_pred):
     return gaussian_nll(y_true, K.concatenate([mu, sigma], axis=1))
 
 
-def gaussian_nll(y_true, y_pred):    
+def gaussian_nll(y_true, y_pred):
     n_dims = int(int(y_pred.shape[1]) / 2)
     mu = y_pred[:, :n_dims]
     logsigma = y_pred[:, n_dims:]
@@ -30,11 +30,11 @@ def gaussian_nll(y_true, y_pred):
     #     loss += tf.tensordot(tf.tensordot(K.transpose(diff[:, i]), inv_s, 1), diff[:, i], 1) + logdet_sigma[i]
     # return loss
 
-    mse = -0.5 * K.sum(K.square((y_true - mu) / K.exp(logsigma)), axis=1)
+    f = -0.5 * K.sum(K.square((y_true - mu) / K.exp(logsigma)), axis=1)
     sigma_trace = -K.sum(logsigma, axis=1)
     log2pi = -0.5 * n_dims * np.log(2 * np.pi)
-    
-    log_likelihood = mse + sigma_trace + log2pi
+
+    log_likelihood = f + sigma_trace + log2pi
 
     return K.mean(-log_likelihood)
 

probabilistic_model.py
@@ -21,7 +21,7 @@ def load(exp_path, fname):
     return data, files
 
 
-def split_polar(data, timestep, args={'center' : (0, 0)}):
+def split_polar(data, timestep, args={'center': (0, 0)}):
     if 'center' not in args.keys():
         args['center'] = (0, 0)
 
@@ -32,11 +32,12 @@ def split_polar(data, timestep, args={'center' : (0, 0)}):
     for p in pos:
         for n in range(p.shape[1] // 2):
             pos_t = np.roll(p, shift=1, axis=0)[2:, :]
-            pos_t_1 = np.roll(p, shift=1, axis=0)[1:-1, :] 
+            pos_t_1 = np.roll(p, shift=1, axis=0)[1:-1, :]
             vel_t = (p - np.roll(p, shift=1, axis=0))[2:, :] / timestep
             vel_t_1 = (p - np.roll(p, shift=1, axis=0))[1:-1, :] / timestep
 
-            X = np.array([pos_t_1[:, 0], pos_t_1[:, 1], vel_t_1[:, 0], vel_t_1[:, 1]])
+            X = np.array([pos_t_1[:, 0], pos_t_1[:, 1],
+                          vel_t_1[:, 0], vel_t_1[:, 1]])
             Y = np.array([vel_t[:, 0], vel_t[:, 1]])
             if inputs is None:
                 inputs = X
@@ -73,7 +74,7 @@ if __name__ == '__main__':
 
     pos, _ = load(args.path, 'positions_filtered.dat')
     data = {
-        'pos' : pos,
+        'pos': pos,
     }
     X, Y = split_data(data, args.timestep)
     X = X.transpose()
@@ -85,29 +86,30 @@ if __name__ == '__main__':
 
     model = tf.keras.Sequential()
     model.add(tf.keras.layers.Flatten(input_shape=(x_train.shape[1],)))
-    model.add(tf.keras.layers.Dense(40, activation='tanh')) 
-    # model.add(tf.keras.layers.Dense(40, activation='tanh')) 
-    model.add(tf.keras.layers.Dense(Y.shape[1]*2, activation='tanh'))
+    model.add(tf.keras.layers.Dense(30, activation='tanh'))
+    model.add(tf.keras.layers.Dense(30, activation='tanh'))
+    model.add(tf.keras.layers.Dense(30, activation='tanh'))
+    model.add(tf.keras.layers.Dense(Y.shape[1]*2, activation=None))
 
-    loss = gaussian_nll_tanh
+    loss = gaussian_nll
     optimizer = tf.keras.optimizers.Adam(0.0001)
     model.compile(loss=loss,
-                    optimizer=optimizer,
-                    metrics=[gaussian_mse, gaussian_mae]
-                    )
+                  optimizer=optimizer,
+                  metrics=[gaussian_mse, gaussian_mae]
+                  )
     model.summary()
 
     for epoch in range(args.epochs):
         model.fit(x_train, y_train,
-         batch_size=args.batch_size, 
-         epochs=epoch+1, 
-         initial_epoch=epoch,
-         validation_data=(x_val, y_val),
-         verbose=1)
+                  batch_size=args.batch_size,
+                  epochs=epoch+1,
+                  initial_epoch=epoch,
+                  validation_data=(x_val, y_val),
+                  verbose=1)
 
         if epoch % args.dump == 0:
-            model.save(str(Path(args.path).joinpath('prob_' + str(epoch) + '_model.h5')))
-
+            model.save(str(Path(args.path).joinpath(
+                'prob_' + str(epoch) + '_model.h5')))
 
     # model.fit(x_train, y_train,
     #             batch_size=args.batch_size,
@@ -117,4 +119,3 @@ if __name__ == '__main__':
     #             )
 
     model.save(str(Path(args.path).joinpath('prob_model.h5')))
-    

probabilistic_sim.py
@@ -48,7 +48,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     model = tf.keras.models.load_model(Path(args.path).joinpath(args.model + '_model.h5'), custom_objects={
-                                       'gaussian_nll_tanh': gaussian_nll_tanh, 'gaussian_mse': gaussian_mse, 'gaussian_mae': gaussian_mae})
+                                       'gaussian_nll': gaussian_nll, 'gaussian_mse': gaussian_mse, 'gaussian_mae': gaussian_mae})
     setup = CircularCorridor()
 
     inputs = None
@@ -99,9 +99,9 @@ if __name__ == '__main__':
         failed = 0
         while True:
             sample_velx = np.random.normal(
-                prediction[0, 0], prediction[0, 2] / 30, 1)[0]
+                prediction[0, 0], prediction[0, 2], 1)[0]
             sample_vely = np.random.normal(
-                prediction[0, 1], prediction[0, 3] / 30, 1)[0]
+                prediction[0, 1], prediction[0, 3], 1)[0]
 
             x_hat = generated_data[-1, 0] + sample_velx * args.timestep
             y_hat = generated_data[-1, 1] + sample_vely * args.timestep

rnn_model.py
@@ -20,7 +20,7 @@ def load(exp_path, fname):
     return data, files
 
 
-def split_polar(data, timestep, args={'center' : (0, 0)}):
+def split_polar(data, timestep, args={'center': (0, 0)}):
     if 'center' not in args.keys():
         args['center'] = (0, 0)
 
@@ -31,11 +31,12 @@ def split_polar(data, timestep, args={'center' : (0, 0)}):
     for p in pos:
         for n in range(p.shape[1] // 2):
             pos_t = np.roll(p, shift=1, axis=0)[2:, :]
-            pos_t_1 = np.roll(p, shift=1, axis=0)[1:-1, :] 
+            pos_t_1 = np.roll(p, shift=1, axis=0)[1:-1, :]
             vel_t = (p - np.roll(p, shift=1, axis=0))[2:, :] / timestep
             vel_t_1 = (p - np.roll(p, shift=1, axis=0))[1:-1, :] / timestep
 
-            X = np.array([pos_t_1[:, 0], pos_t_1[:, 1], vel_t_1[:, 0], vel_t_1[:, 1]])
+            X = np.array([pos_t_1[:, 0], pos_t_1[:, 1],
+                          vel_t_1[:, 0], vel_t_1[:, 1]])
             Y = np.array([vel_t[:, 0], vel_t[:, 1]])
             if inputs is None:
                 inputs = X
@@ -72,7 +73,7 @@ if __name__ == '__main__':
 
     pos, _ = load(args.path, 'positions_filtered.dat')
     data = {
-        'pos' : pos,
+        'pos': pos,
     }
     X, Y = split_data(data, args.timestep)
     X = X.transpose()
@@ -89,31 +90,32 @@ if __name__ == '__main__':
     x_val = np.reshape(x_val, (x_val.shape[0], 1, x_val.shape[1]))
 
     model = tf.keras.Sequential()
-    model.add(tf.keras.layers.LSTM(40, return_sequences=False,
-                input_shape=(timesteps, X.shape[1])))  # returns a sequence of vectors of dimension 30
-    # model.add(tf.keras.layers.LSTM(30, return_sequences=False))  # returns a sequence of vectors of dimension 30
-    # model.add(tf.keras.layers.LSTM(30))  # return a single vector of dimension 30
-    model.add(tf.keras.layers.Dense(Y.shape[1]*2, activation='tanh'))
+    model.add(tf.keras.layers.LSTM(30, return_sequences=True,
+                                   input_shape=(timesteps, X.shape[1])))
+    model.add(tf.keras.layers.LSTM(30, return_sequences=True,
+                                   input_shape=(timesteps, X.shape[1])))
+    model.add(tf.keras.layers.LSTM(30, return_sequences=False,
+                                   input_shape=(timesteps, X.shape[1])))
+    model.add(tf.keras.layers.Dense(Y.shape[1]*2, activation=None))
     # model.add(tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(y_train.shape[1], activation='tanh')))
 
     optimizer = tf.keras.optimizers.Adam(0.0001)
     model.compile(loss=gaussian_nll_tanh,
-                    optimizer=optimizer,
-                    metrics=[gaussian_mse, gaussian_mae])
+                  optimizer=optimizer,
+                  metrics=[gaussian_mse, gaussian_mae])
     model.summary()
-
 
     for epoch in range(args.epochs):
         model.fit(x_train, y_train,
-         batch_size=args.batch_size, 
-         epochs=epoch+1, 
-         initial_epoch=epoch,
-         validation_data=(x_val, y_val),
-         verbose=1)
+                  batch_size=args.batch_size,
+                  epochs=epoch+1,
+                  initial_epoch=epoch,
+                  validation_data=(x_val, y_val),
+                  verbose=1)
 
         if epoch % args.dump == 0:
-            model.save(str(Path(args.path).joinpath('rnn_' + str(epoch) + '_model.h5')))
-
+            model.save(str(Path(args.path).joinpath(
+                'rnn_' + str(epoch) + '_model.h5')))
 
     # model.fit(x_train, y_train,
     #             batch_size=args.batch_size,
@@ -123,4 +125,3 @@ if __name__ == '__main__':
     #             )
 
     model.save(str(Path(args.path).joinpath('rnn_model.h5')))
-    