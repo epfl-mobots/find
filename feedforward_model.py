6e15b13d3a8c708574f2da52159e68c882ed364a
Vaios Papaspyros <b.papaspyros@gmail.com>
2019-07-07 20:37:59 +0200

    Implement feedforward model for baseline comparisons

---
+++ feedforward_model.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+
+import glob
+import argparse
+import numpy as np
+from pathlib import Path
+
+import tensorflow as tf
+import tensorflow.keras.backend as K
+
+from utils import angle_to_pipi
+from losses import *
+
+
+def load(exp_path, fname):
+    files = glob.glob(exp_path + '/*' + fname)
+    data = []
+    for f in files:
+        matrix = np.loadtxt(f)
+        data.append(matrix)
+    return data, files
+
+
+def split_polar(data, timestep, args={'center': (0, 0)}):
+    if 'center' not in args.keys():
+        args['center'] = (0, 0)
+
+    pos = data['pos']
+
+    inputs = None
+    outputs = None
+    for p in pos:
+        for n in range(p.shape[1] // 2):
+            pos_t = np.roll(p, shift=1, axis=0)[2:, :]
+            pos_t_1 = np.roll(p, shift=1, axis=0)[1:-1, :]
+            vel_t = (p - np.roll(p, shift=1, axis=0))[2:, :] / timestep
+            vel_t_1 = (p - np.roll(p, shift=1, axis=0))[1:-1, :] / timestep
+
+            X = np.array([pos_t_1[:, 0], pos_t_1[:, 1],
+                          vel_t_1[:, 0], vel_t_1[:, 1]])
+            Y = np.array([vel_t[:, 0], vel_t[:, 1]])
+            if inputs is None:
+                inputs = X
+                outputs = Y
+            else:
+                inputs = np.append(inputs, X, axis=1)
+                outputs = np.append(outputs, Y, axis=1)
+    return inputs, outputs
+
+
+def split_data(data, timestep, split_func=split_polar, args={}):
+    return split_func(data, timestep, args)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Dense model to reproduce fish motion')
+    parser.add_argument('--path', '-p', type=str,
+                        help='Path to the experiment',
+                        required=True)
+    parser.add_argument('--timestep', '-t', type=float,
+                        help='Simulation timestep',
+                        required=True)
+    parser.add_argument('--epochs', '-e', type=int,
+                        help='Number of training epochs',
+                        default=1000)
+    parser.add_argument('--batch_size', '-b', type=int,
+                        help='Batch size',
+                        default=256)
+    parser.add_argument('--dump', '-d', type=int,
+                        help='Batch size',
+                        default=100)
+    args = parser.parse_args()
+
+    pos, _ = load(args.path, 'positions_filtered.dat')
+    data = {
+        'pos': pos,
+    }
+    X, Y = split_data(data, args.timestep)
+    X = X.transpose()
+    Y = Y.transpose()
+
+    split_at = X.shape[0] - X.shape[0] // 10
+    (x_train, x_val) = X[:split_at, :], X[split_at:, :]
+    (y_train, y_val) = Y[:split_at, :], Y[split_at:, :]
+
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Flatten(input_shape=(x_train.shape[1],)))
+    model.add(tf.keras.layers.Dense(50, activation='tanh'))
+    model.add(tf.keras.layers.Dense(50, activation='tanh'))
+    model.add(tf.keras.layers.Dense(Y.shape[1], activation=None))
+
+    optimizer = tf.keras.optimizers.Adam(0.0001)
+    model.compile(loss='mse',
+                  optimizer=optimizer,
+                  metrics=['mae']
+                  )
+    model.summary()
+
+    for epoch in range(args.epochs):
+        model.fit(x_train, y_train,
+                  batch_size=args.batch_size,
+                  epochs=epoch+1,
+                  initial_epoch=epoch,
+                  validation_data=(x_val, y_val),
+                  verbose=1)
+
+        if epoch % args.dump == 0:
+            model.save(str(Path(args.path).joinpath(
+                'ffw_' + str(epoch) + '_model.h5')))
+
+    # model.fit(x_train, y_train,
+    #             batch_size=args.batch_size,
+    #             epochs=args.epochs,
+    #             verbose=1,
+    #             validation_data=(x_val, y_val)
+    #             )
+
+    model.save(str(Path(args.path).joinpath('ffw_model.h5')))
---
+++ feedforward_sim.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+
+import glob
+import argparse
+import numpy as np
+from pathlib import Path
+
+import tensorflow as tf
+import tensorflow.keras.backend as K
+
+from features import Velocities
+from utils import angle_to_pipi
+
+
+from losses import *
+
+
+class CircularCorridor:
+    def __init__(self, radius=1.0, center=(0, 0)):
+        self._center = center
+        self._radius = radius
+
+    def is_valid(self, radius):
+        return radius < self._radius and radius > 0
+
+    def center(self):
+        return self._center
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Probabilistic model to reproduce fish motion')
+    parser.add_argument('--path', '-p', type=str,
+                        help='Path to the experiment',
+                        required=True)
+    parser.add_argument('--reference', '-r', type=str,
+                        help='Path to a reference experiment position file',
+                        required=True)
+    parser.add_argument('--model', '-m', type=str,
+                        help='Model file name to use',
+                        required=True)
+    parser.add_argument('--iterations', '-i', type=int,
+                        help='Number of iteration of the simulation',
+                        required=True)
+    parser.add_argument('--timestep', '-t', type=float,
+                        help='Simulation timestep',
+                        required=True)
+    args = parser.parse_args()
+
+    model = tf.keras.models.load_model(Path(args.path).joinpath(args.model + '_model.h5'))
+    setup = CircularCorridor()
+
+    inputs = None
+    outputs = None
+    ref_positions = np.loadtxt(args.reference)
+    timestep = args.timestep
+    for n in range(ref_positions.shape[1] // 2):
+        pos_t = np.roll(ref_positions, shift=1, axis=0)[2:, :]
+        pos_t_1 = np.roll(ref_positions, shift=1, axis=0)[1:-1, :]
+        vel_t = (ref_positions - np.roll(ref_positions,
+                                         shift=1, axis=0))[2:, :] / timestep
+        vel_t_1 = (ref_positions - np.roll(ref_positions,
+                                           shift=1, axis=0))[1:-1, :] / timestep
+
+        X = np.array([pos_t_1[:, 0], pos_t_1[:, 1],
+                      vel_t_1[:, 0], vel_t_1[:, 1]])
+        Y = np.array([vel_t[:, 0], vel_t[:, 1]])
+        if inputs is None:
+            inputs = X
+            outputs = Y
+        else:
+            inputs = np.append(inputs, X, axis=1)
+            outputs = np.append(outputs, Y, axis=1)
+    X = X.transpose()
+    Y = Y.transpose()
+
+    generated_data = np.matrix([X[0, 0], X[0, 1]])
+    for t in range(args.iterations-1):
+        print('Current timestep: ' + str(t))
+
+        if t == 0:
+            prediction = np.array(model.predict(X[0].reshape(1, X.shape[1])))
+        else:
+            dvel_t = (generated_data[-1, :] -
+                      generated_data[-2, :]) / args.timestep
+            nninput = np.array(
+                [generated_data[-1, 0], generated_data[-1, 1], dvel_t[0, 0], dvel_t[0, 1]]).transpose()
+            prediction = np.array(model.predict(
+                nninput.reshape(1, X.shape[1])))
+
+        failed = 0
+        while True:
+            x_hat = generated_data[-1, 0] + prediction[0, 0] * args.timestep
+            y_hat = generated_data[-1, 1] + prediction[0, 1] * args.timestep
+
+            r = np.sqrt(
+                (x_hat - setup.center()[0]) ** 2 + (y_hat - setup.center()[1]) ** 2)
+            if setup.is_valid(r):
+                generated_data = np.vstack([generated_data, [x_hat, y_hat]])
+                break
+            else:
+                rold = np.sqrt((generated_data[-1, 0] - setup.center()[0]) ** 2 + (
+                    generated_data[-1, 1] - setup.center()[1]) ** 2)
+                failed += 1
+                if failed > 999:
+                    generated_data = np.vstack([generated_data, generated_data[-1, :]])
+                    break
+
+    gp_fname = args.reference.replace('processed', 'generated')
+    gv_fname = gp_fname.replace('positions', 'velocities')
+    gv = Velocities([np.array(generated_data)], args.timestep).get()
+
+    np.savetxt(gp_fname, generated_data)
+    np.savetxt(gv_fname, gv[0])
