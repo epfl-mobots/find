078c6a79943f91c1f63d7eb19ab8633bf05d7535
Vaios Papaspyros <b.papaspyros@gmail.com>
2019-08-05 08:55:44 +0200

    Add plot script for comparing trajectories and velocities

---
+++ scripts/plot_smoothing_results.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import os
+import glob
+import argparse
+import numpy as np
+from pathlib import Path
+from pprint import pprint
+
+import scipy.signal as signal
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Plot two trajectory signals against each other')
+    parser.add_argument('--path1', 
+                        type=str,
+                        help='Path to the first trajectory file',
+                        required=True)
+    parser.add_argument('--path2', 
+                        type=str,
+                        help='Path to the second trajectory file',
+                        required=True)
+    args = parser.parse_args()
+
+    p1 = np.loadtxt(args.path1)
+    p2 = np.loadtxt(args.path2)
+
+    v1 = np.loadtxt(args.path1.replace('positions', 'velocities'))
+    v2 = np.loadtxt(args.path2.replace('positions', 'velocities'))
+
+    plt.plot(p1[:, 0])
+    plt.plot(p2[:, 0])
+    plt.show()
+
+    plt.plot(p1[:, 1])
+    plt.plot(p2[:, 1])
+    plt.show()
+
+    plt.plot(v1[:, 0])
+    plt.plot(v2[:, 0])
+    plt.show()
+
+    plt.plot(v1[:, 1])
+    plt.plot(v2[:, 1])
+    plt.show()
+
+
