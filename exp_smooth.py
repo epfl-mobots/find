a7e4e48251502401034db80c6d5dded6ac5f5575
Vaios Papaspyros <b.papaspyros@gmail.com>
2019-08-05 08:51:45 +0200

    Implement exponential smoothing filter for the trajectories

--- exp_smooth.py
+++ exp_smooth.py
@@ -9,9 +9,21 @@ from features import Velocities
 from utils import ExperimentInfo, Center, Normalize
 
 
+def exp_filter(signal, alpha):
+    filtered_signal = []
+    for n in range(signal.shape[1]):
+        sig = signal[:, n]
+        filtered_sig = [sig[0]]
+        # y(k) = y(k-1) + (1-a)*( x(k) - y(k-1) )
+        for m in range(1, sig.shape[0]):
+            filtered_sig.append(filtered_sig[-1] + (1-alpha) * (sig[m] - filtered_sig[-1]))
+        filtered_signal.append(filtered_sig)
+    return np.transpose(np.array(filtered_signal))
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description='Smoothing for the fish trajectories')
+        description='Exponential smoothing for the fish trajectories')
     parser.add_argument('--path', '-p', type=str,
                         help='Path to the experiment',
                         required=True)
@@ -21,11 +33,16 @@ if __name__ == '__main__':
     parser.add_argument('--centroids', '-c', type=int,
                         help='Frames to use in order to compute the centroidal positions',
                         required=True)
+    parser.add_argument('--alpha', '-a', type=float,
+                        default=0.01, 
+                        help='Smoothing factor',
+                        required=False)
     parser.add_argument('--center', action='store_true',
                         help='Center smoothed data')
     parser.add_argument('--norm', action='store_true',
                         help='Normalize smoothed data')
     args = parser.parse_args()
+
 
     timestep = args.centroids / args.fps
 
@@ -33,16 +50,7 @@ if __name__ == '__main__':
     data = []
     for f in files:
         positions = np.loadtxt(f)
-
-        mus = []
-        for i in range(positions.shape[1] // 2):
-            kf = KalmanFilter(n_dim_state=2, n_dim_obs=2)
-            mu, sigma = kf.filter(positions[:, i*2:i*2+2])
-            if len(mus) == 0:
-                mus = np.array(mu)
-            else:
-                mus = np.append(mus, np.array(mu))
-        data.append(mus)
+        data.append(exp_filter(positions, args.alpha))
 
     info = ExperimentInfo(data)
     if args.center:
