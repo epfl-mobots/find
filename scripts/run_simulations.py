ac3b97a232f56bdeb6072dc992bce9dae7bff312
Vaios Papaspyros <b.papaspyros@gmail.com>
2020-05-18 16:48:22 +0200

    Update run_simulations script to include lstm models

--- scripts/run_simulations.py
+++ scripts/run_simulations.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from multiprocessing import Pool
 
 
-models = ['prob', 'ffw', 'rnn', 'prob_multi']
+models = ['prob', 'ffw', 'rnn', 'prob_multi', 'lstm', 'prob_lstm']
 model_map = {
     'prob': 'prob',
     'prob_lstm': 'prob_lstm',
@@ -22,11 +22,14 @@ script_map = {
     'ffw': 'sim/feedforward_sim.py',
     'rnn': 'sim/rnn_sim',
     'prob_multi': 'sim/probabilistic_multi_sim.py',
+    'prob_lstm': 'sim/probabilistic_lstm_sim.py',
+    'lstm': 'sim/lstm_sim.py',
 }
 
 
 def run_process(process):
     os.system('python {} >/dev/null 2>&1'.format(process))
+    # os.system('python {}'.format(process))
 
 
 if __name__ == '__main__':
@@ -35,16 +38,10 @@ if __name__ == '__main__':
     parser.add_argument('--path', '-p', type=str,
                         help='Path to the experiment',
                         required=True)
-    parser.add_argument('--reference', '-r', type=str,
-                        help='Path to a reference experiment position file',
-                        required=True)
-    parser.add_argument('--iterations', '-i', type=int,
-                        help='Number of iteration of the simulation',
-                        required=False,
-                        default=-1)
-    parser.add_argument('--timestep', '-t', type=float,
-                        help='Simulation timestep',
-                        required=True)
+    parser.add_argument('--arglist', type=str,
+                        help='Argument list to forward to the corresponding',
+                        default='',
+                        required=False)
     parser.add_argument('--model', '-m',
                         type=str,
                         choices=models)
@@ -57,7 +54,7 @@ if __name__ == '__main__':
     for f in files:
         m = model_map[args.model]
         exp_arglist = ' -p ' + args.path + ' -r ' + f + \
-            ' -m ' + m + ' -t ' + str(args.timestep)
+            ' -m ' + m + ' ' + args.arglist
         arglist.append(exp_arglist)
 
     processes = [script_map[args.model]] * len(files)
