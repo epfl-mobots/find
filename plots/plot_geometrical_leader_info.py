7aef9de0651a520ea2f36ed55020774cf152671d
Vaios Papaspyros <b.papaspyros@gmail.com>
2020-06-12 21:12:20 +0200

    Generate a boxlplot for geometrical leadership

--- plots/plot_geometrical_leader_info.py
+++ plots/plot_geometrical_leader_info.py
@@ -5,10 +5,85 @@ import glob
 import matplotlib.lines as mlines
 import seaborn as sns
 from pylab import *
-# from PIL import Image
 
-# image_path = '../plots/fish_dark.png'
-# image = Image.open(image_path)
+flatui = ["#9b59b6", "#3498db", "#95a5a6", "#e74c3c", "#34495e", "#2ecc71"]
+# palette = flatui
+# palette = 'Paired'
+# palette = 'husl'
+# palette = 'Set2'
+palette = sns.cubehelix_palette(11)
+colors = sns.color_palette(palette)
+sns.set(style="darkgrid")
+
+gfontsize = 10
+params = {
+    'axes.labelsize': gfontsize,
+    'font.size': gfontsize,
+    'legend.fontsize': gfontsize,
+    'xtick.labelsize': gfontsize,
+    'ytick.labelsize': gfontsize,
+    'text.usetex': False,
+    # 'figure.figsize': [10, 15]
+    # 'ytick.major.pad': 4,
+    # 'xtick.major.pad': 4,
+    'font.family': 'Arial',
+}
+rcParams.update(params)
+
+pts = np.linspace(0, np.pi * 2, 24)
+circ = np.c_[np.sin(pts) / 2, -np.cos(pts) / 2]
+vert = np.r_[circ, circ[::-1] * 1.0]
+open_circle = mpl.path.Path(vert)
+
+extra = Rectangle((0, 0), 1, 1, fc="w", fill=False,
+                  edgecolor='none', linewidth=0)
+
+shapeList = []
+
+v = np.r_[circ, circ[::-1] * 0.6]
+oc = mpl.path.Path(v)
+
+handles_a = [
+    mlines.Line2D([0], [0], color='black', marker=oc,
+                  markersize=6, label='Mean and SD'),
+    mlines.Line2D([], [], linestyle='none', color='black', marker='*',
+                  markersize=5, label='Median'),
+    mlines.Line2D([], [], linestyle='none', markeredgewidth=1, marker='o',
+                  color='black', markeredgecolor='w', markerfacecolor='black', alpha=0.5,
+                  markersize=5, label='Single run')
+]
+handles_b = [
+    mlines.Line2D([0], [1], color='black', label='Mean'),
+    Circle((0, 0), radius=1, facecolor='black', alpha=0.35, label='SD')
+]
+
+
+def bplots(data, ax, exp_title='', ticks=False):
+    ax = sns.boxplot(data=data, width=0.25, notch=False,
+                     whis=0.8, saturation=1, linewidth=1.0, ax=ax)
+    # ax = sns.violinplot(data=data, width=0.25, notch=False, whis=1.5, saturation=1, ax=ax)
+    # sns.swarmplot(data=data, edgecolor='white', linewidth=0.8, size=2)
+
+
+def pplots(data, ax, sub_colors=[], exp_title='', ticks=False):
+    paper_rc = {'lines.linewidth': 1, 'lines.markersize': 10}
+    sns.set_context("paper", rc=paper_rc)
+
+    sns.pointplot(data=np.transpose(data), palette=sub_colors,
+                  size=5, estimator=np.mean,
+                  ci='sd', capsize=0.2, linewidth=0.8, markers=[open_circle],
+                  scale=1.6, ax=ax)
+
+    sns.stripplot(data=np.transpose(data), edgecolor='white',
+                  dodge=True, jitter=True,
+                  alpha=.50, linewidth=0.8, size=5, palette=sub_colors, ax=ax)
+
+    medians = []
+    for d in data:
+        medians.append([np.median(list(d))])
+    sns.swarmplot(data=medians, palette=['#000000'] * 10,
+                  marker='*', size=5, ax=ax)
+
 
 def angle_to_pipi(dif):
     while True:
@@ -34,28 +109,14 @@ def compute_leadership(positions, velocities):
     leadership_timeseries = []
 
     for i in range(vel.shape[0]):
-        # fig = plt.figure(figsize=(5, 5))
-        # ax = plt.gca()
-        # outer = plt.Circle(
-        #     (0, 0), 1.0, color='black', fill=False)
-        # ax.add_artist(outer)
-
         angles = []
-        phis = []
 
         for inum, j in enumerate(range(positions.shape[1] // 2)):
             x = positions[i, j * 2]
             y = positions[i, j * 2 + 1]
 
-            phi = angle_to_pipi(np.arctan2(velocities[i, j * 2 + 1],
-                                velocities[i, j * 2])) 
-            phis.append(phi)
-
+            phi = angle_to_pipi(np.arctan2(velocities[i, j * 2 + 1], velocities[i, j * 2])) 
             angles.append((angle_to_pipi(phi - angle_list[j][i]) + np.pi) % (2 * np.pi) - np.pi)
-
-            # rotated_img = image.rotate(phi * 180 / np.pi)
-            # ax.imshow(rotated_img, extent=[x - 0.06, x + 0.06, y - 0.06, y + 0.06], aspect='equal')
-            # plt.text(x + 0.025, y + 0.025, str(j), color='r', fontsize=5)
 
         geo_leader = np.argmin(angles)
         if geo_leader != previous_leader:
@@ -63,32 +124,35 @@ def compute_leadership(positions, velocities):
             previous_leader = geo_leader
         leadership_timeseries.append([i, geo_leader])
 
-
-        # TODO: this is legacy code for visualising the angles and validating the script is correct
-        # it should be removed in the future
-
-        # ax.axis('off')
-        # ax.set_xlim([-1.1, 1.1])
-        # ax.set_ylim([-1.1, 1.1])
-        # plt.tight_layout()
-
-        # angles = list(map(lambda x: abs(x * 180) / np.pi, angles))
-        # plt.text(0.9, 0.9, 'Leader: ' + str(geo_leader), color='r', fontsize=5)
-        # plt.text(0.6, 0.8, 'Ang0: ' + str((ang0[i] * 180) / np.pi), color='r', fontsize=5)
-        # plt.text(0.6, 0.7, 'Ang1: ' + str((ang1[i] * 180) / np.pi), color='r', fontsize=5)
-        # plt.text(0.6, 0.6, 'Angle0: ' + str(angles[0]), color='r', fontsize=5)
-        # plt.text(0.6, 0.5, 'Angle1: ' + str(angles[1]), color='r', fontsize=5)
-        # plt.text(0.6, 0.4, 'Heading0: ' + str((phis[0] * 180) / np.pi), color='r', fontsize=5)
-        # plt.text(0.6, 0.3, 'Heading1: ' + str((phis[1] * 180) / np.pi), color='r', fontsize=5)
-        # png_fname = str(i).zfill(6)
-        # plt.savefig(
-        #     str(png_fname) + '.png',
-        #     transparent=True,
-        #     dpi=300
-        # )
-        # plt.close('all')
-
     return (leader_changes, leadership_timeseries)
+
+
+def compute_consecutive(timeseries):
+    occurences = []
+    i = 1
+    count = 1
+    while i < timeseries.shape[0]:
+        if timeseries[i] == timeseries[i-1]:
+            count += 1
+        else:
+            occurences.append(count)
+            count = 1
+        i += 1
+    occurences.append(count)
+    return (np.mean(occurences), occurences)
+
+
+def plot_consecutive(occurences, filename):
+    fig = plt.figure(figsize=(15, 5))
+    ax = plt.gca()
+
+    bplots(occurences, ax)
+
+    plt.savefig(
+        filename + '.png',
+        transparent=False,
+        dpi=300
+    )
 
 
 if __name__ == '__main__':
@@ -96,14 +160,29 @@ if __name__ == '__main__':
     parser.add_argument('--path', '-p', type=str,
                         help='Path to data directory',
                         required=True)
+    parser.add_argument('--regex', action='store_true',
+                        help='Flag to signify that args.positions is a regex',
+                        default=False)
     args = parser.parse_args()
 
-    files = glob.glob(args.path + '/*processed_velocities.dat')
+
+    files = []
+    if not args.regex:
+        files.append(args.path)
+    else:
+        files = glob.glob(args.path)
 
     leader_change_count = 0
+    oc_list = []
     for f in files: 
         vel = np.loadtxt(f)
         pos = np.loadtxt(f.replace('velocities', 'positions'))
         (leader_change_count, leadership_timeseries) = compute_leadership(pos, vel)
-        np.savetxt(f.replace('velocities', 'leadership_info'), np.array(leadership_timeseries)) 
+        leadership_timeseries = np.array(leadership_timeseries)
+        np.savetxt(f.replace('velocities', 'leadership_info'), leadership_timeseries) 
         np.savetxt(f.replace('velocities', 'leadership_change_count'), np.array([leader_change_count])) 
+
+        (avg, occurences) = compute_consecutive(leadership_timeseries[:, 1])
+        oc_list.append(occurences)
+
+    plot_consecutive(oc_list, 'geo_leader_bplot')
