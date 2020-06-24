Calculation fixes for resultant quantities

plots/plot_distance_to_wall.py
@@ -86,7 +86,7 @@ def distance_plot(data, experiments):
     fig.subplots_adjust(hspace=0.05, wspace=0.10)
     sns.despine(bottom=True, left=True)
 
-    ylim = [0, 50]
+    ylim = [0, 60]
 
     for i, k in enumerate(sorted(data.keys())):
         vectors = data[k]
@@ -108,13 +108,11 @@ def distance_plot(data, experiments):
             cax.set_xticklabels([])
         cax.set_xticks(np.arange(0.00, 0.305, 0.05))
         cax.set_ylim(ylim)
+        cax.set_xlim([-0.08, 0.40])
 
     cax = ax
     if num_experiments > 1:
         cax = ax[0]
-
-    # cax.set_xlabel('Velocity (m/s)')
-    # cax.set_ylabel('Frequency')
 
     fig.text(0.5, 0.08, 'Distance (m)', ha='center', va='center')
     fig.text(0.06, 0.5, 'Frequency', ha='center',
@@ -166,10 +164,10 @@ if __name__ == '__main__':
         vel = glob.glob(args.path + '/' + experiments[e])
         for v in vel:
             # TODO: this is to convert to meters but I should probably do this in a cleaner way
-            matrix = np.loadtxt(v) * 0.29
+            matrix = np.loadtxt(v) * 0.25
             distances = np.array((matrix.shape[0], 1))
             for i in range(matrix.shape[1] // 2):
-                distance = 0.29 - \
+                distance = 0.25 - \
                            np.sqrt(matrix[:, i * 2] ** 2 + matrix[:, i * 2 + 1] ** 2)
                 data[e].append(distance)
 

plots/plot_grid_occupancy.py
@@ -42,11 +42,11 @@ if __name__ == '__main__':
         center, radius[0], color='white', fill=False)
     outer = plt.Circle(
         center, radius[1], color='white', fill=False)
-    if not args.open:
-        ax.add_artist(inner)
-    ax.add_artist(outer)
+    # if not args.open:
+    #     ax.add_artist(inner)
+    # ax.add_artist(outer)
 
-    bins = 120
+    bins = 220
     y, x = np.meshgrid(np.linspace(center[0] - (oradius + 0.0001),
                                    center[0] + (oradius + 0.0001), bins),
                        np.linspace(center[1] - (oradius + 0.0001),

plots/plot_res_acc_distribution.py
@@ -160,7 +160,7 @@ if __name__ == '__main__':
 
             for i in range(acceleration.shape[1] // 2):
                 linear_acceleration = np.sqrt(acceleration[:, i * 2] ** 2 + acceleration[:, i * 2 + 1] ** 2
-                                              - 2 * acceleration[:, i * 2] * acceleration[:, i * 2 + 1] * np.cos(
+                                              - 2 * np.abs(acceleration[:, i * 2]) * np.abs(acceleration[:, i * 2 + 1]) * np.cos(
                     np.arctan2(acceleration[:, i * 2 + 1], acceleration[:, i * 2])))
                 data[e].append(linear_acceleration)
 

plots/plot_res_vel_distribution.py
@@ -152,7 +152,7 @@ if __name__ == '__main__':
             linear_velocity = np.array((matrix.shape[0], 1))
             for i in range(matrix.shape[1] // 2):
                 linear_velocity = np.sqrt(matrix[:, i * 2] ** 2 + matrix[:, i * 2 + 1] ** 2
-                                          - 2 * matrix[:, i * 2] * matrix[:, i * 2 + 1] * np.cos(
+                                          - 2 * np.abs(matrix[:, i * 2]) * np.abs(matrix[:, i * 2 + 1]) * np.cos(
                     np.arctan2(matrix[:, i * 2 + 1], matrix[:, i * 2])))
                 data[e].append(linear_velocity)
 

plots/plot_traj.py
@@ -52,7 +52,7 @@ if __name__ == '__main__':
     for i in range(tsteps):
         r = np.sqrt(velocities[i, args.ind * 2 + 1] ** 2 +
                     velocities[i, args.ind * 2] ** 2 -
-                    2 * velocities[i, args.ind * 2 + 1] * velocities[i, args.ind * 2] * np.cos(
+                    2 * np.abs(velocities[i, args.ind * 2 + 1]) * np.abs(velocities[i, args.ind * 2]) * np.cos(
             np.arctan2(velocities[i, args.ind * 2 + 1], velocities[i, args.ind * 2])))
         rvelocities.append(r)
     print('Mean:', np.mean(rvelocities))

plots/visualize_trajectories.py
@@ -126,7 +126,7 @@ if __name__ == '__main__':
                     #     x, y, vel[i, j * 2], vel[i, j * 2 + 1], scale=1, units='xy', color='r')
 
                     rvel = np.sqrt((vel[i, j * 2] * args.velocity_coef) ** 2 + (vel[i, j * 2 + 1] * args.velocity_coef)
-                                   ** 2 - 2 * vel[i, j * 2] * args.velocity_coef * vel[i, j * 2 + 1] * args.velocity_coef * np.cos(np.arctan2(vel[i, j * 2 + 1], vel[i, j * 2])))
+                                   ** 2 - 2 * np.abs(vel[i, j * 2] * args.velocity_coef) * np.abs(vel[i, j * 2 + 1] * args.velocity_coef) * np.cos(np.arctan2(vel[i, j * 2 + 1], vel[i, j * 2])))
                     plt.text(x + 0.025, y + 0.025,
                              "{:.4f}".format(rvel) + ' m/s', color='r', fontsize=5)
 