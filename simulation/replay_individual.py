4d5141b6e1013597489b8742d06d432e450aec6b
Vaios Papaspyros <b.papaspyros@gmail.com>
2020-08-25 13:17:38 +0200

    Fix replay individual to store history

--- simulation/replay_individual.py
+++ simulation/replay_individual.py
@@ -3,12 +3,17 @@ from simulation.simu.simulation.individual import Individual
 class ReplayIndividual(Individual):
     def __init__(self, positions, velocities):
         super().__init__(is_robot=False) # explicitly setting that this is not a robotic/virtual individual
-        self._positions = positions
-        self._velocities = velocities
+        self._position_history = positions
+        self._velocity_history = velocities
+        self._position = self._position_history[0, :]
+        self._velocity = self._velocity_history[0, :]
 
     def interact(self, simu):
         pass
 
     def move(self, simu):
-        self._position = self._positions[simu.get_current_iteration(), :]
-        self._velocity = self._velocities[simu.get_current_iteration(), :]
+        self._position = self._position_history[simu.get_current_iteration(), :]
+        self._velocity = self._velocity_history[simu.get_current_iteration(), :]
+
+    def _history_update(self, simu):
+        pass
