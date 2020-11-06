43b0b9609887dde2d1dc5d40f25a86e0619fbe6d
Vaios Papaspyros <b.papaspyros@gmail.com>
2020-11-06 17:13:03 +0100

    Add multi dimensional NLL loss

--- utils/losses.py
+++ utils/losses.py
@@ -28,6 +28,39 @@ def gaussian_nll(y_true, y_pred):
     return K.mean(-log_likelihood)
 
 
+def multi_dim_gaussian_nll(y_true, y_pred):
+    """
+    :brief: Gaussian negative log likelihood loss function for probabilistic network outputs.
+
+    :param y_true: np.array of the values the network needs to predict
+    :param y_pred: np.array of the values the network predicted
+    :return: float
+    """
+
+    means = []
+    prediction_steps = y_pred.shape[2]
+    for i in range(prediction_steps):
+        n_dims = int(int(y_pred.shape[3]) / 2)
+        mu = y_pred[:, 0, i, :n_dims]
+        logsigma = y_pred[:, 0, i, n_dims:]
+
+        max_logvar = 0.5
+        min_logvar = -10
+        logsigma = max_logvar - K.log(K.exp(max_logvar - logsigma) + 1)
+        logsigma = min_logvar + K.log(K.exp(logsigma - min_logvar) + 1)
+
+        # https://www.cs.cmu.edu/~epxing/Class/10701-08s/recitation/gaussian.pdf
+        f = -0.5 * \
+            K.sum(K.square((y_true[:, 0, i, :] - mu) /
+                           K.exp(logsigma)), axis=1)
+        sigma_trace = -K.sum(logsigma, axis=1)
+        log2pi = -0.5 * n_dims * np.log(2 * np.pi)
+        log_likelihood = f + sigma_trace + log2pi
+        means.append(K.mean(-log_likelihood))
+
+    return sum(means) / len(means)
+
+
 def gaussian_mae(y_true, y_pred):
     """
     :brief: Custom mean absolute error function for the Gaussian negative log likelihood function.
