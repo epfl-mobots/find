3a34625f7d2295902d3996dde636f0efc1297ba4
Vaios Papaspyros <b.papaspyros@gmail.com>
2020-11-11 18:57:46 +0100

    Requirements are now handled in the setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,46 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-setup(name='find', version='0.1', packages=find_packages())
+# extract version from __init__.py
+with open('__init__.py', 'r') as f:
+    VERSION_LINE = [l for l in f if l.startswith('__version__')][0]
+    VERSION = VERSION_LINE.split('=')[1].strip()[1:-1]
+
+
+setup(
+    name='trajnetbaselines',
+    version=VERSION,
+    packages=find_packages(),
+    description='Fish INteraction moDeling framework.',
+    long_description=open('Readme.md').read(),
+    author='Vaios Papaspyros',
+    author_email='b.papaspyros@gmail.com',
+    url='https://github.com/bpapaspyros/find',
+
+    install_requires=[
+        'numpy',
+        'h5py==2.10.0',
+        'python-dateutil',
+        'tensorflow==1.14.0',
+        'tqdm',
+        'word2number',
+    ],
+    extras_require={
+        'test': [
+            'pylint',
+            'autopep8',
+        ],
+        'plot': [
+            'pandas',
+            'seaborn',
+            'scipy',
+            'matplotlib==3.2.0',
+        ],
+        'gpu': [
+            'tensorflow-gpu==1.14.0',
+        ],
+    },
+
+    classifiers=[
+    ]
+)
