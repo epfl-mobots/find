0149a679e13581876b5888ffa3e01e5c86e68692
Vaios Papaspyros <b.papaspyros@gmail.com>
2020-11-12 15:41:29 +0100

    The simulation script can now handle multiple simus internally

--- find/simulation/simulation.py
+++ find/simulation/simulation.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 
+import os
+import sys
 import argparse
+from tqdm import tqdm
+from multiprocessing import Pool
+
 from find.models.loader import Loader
 from find.models.storage import ModelStorage
 from find.models.model_factory import ModelFactory
 from find.simulation.simulation_factory import available_functors, SimulationFactory, get_most_influential_individual
+
+
+def run_process(process):
+    # os.system('python {}'.format(process))
+    os.system('python {} >/dev/null 2>&1'.format(process))
 
 
 if __name__ == '__main__':
@@ -19,6 +29,10 @@ if __name__ == '__main__':
     parser.add_argument('--load', '-l',
                         type=str,
                         help='Load model from existing file and continue the training process',
+                        required=False)
+    parser.add_argument('--num_proc', '-j', type=int,
+                        help='Number of pool processes to use',
+                        default=16,
                         required=False)
 
     # model selection arguments
@@ -81,13 +95,36 @@ if __name__ == '__main__':
         args.load, 'keras', args)  # TODO: future versions should handle other backends
 
     # read reference data
-    data, _ = loader.load(args.reference, is_absolute=True)
+    data, files = loader.load(args.reference, is_absolute=True)
+    if args.num_proc < 0:
+        args.num_proc = len(data)
 
-    # generate simulator
-    for d in data:
-        simu = simu_factory(d, model, args.nn_functor, 'keras', args)
+    if len(data) == 1:
+        # generate simulator
+        simu = simu_factory(data[0], model, args.nn_functor, 'keras', args)
         if simu is None:
             import warnings
             warnings.warn('Skipping small simulation')
             exit(1)
         simu.spin()
+    else:
+        # in case we have multiple reference files then we create a number
+        # of parallel process that will each simulate different reference
+        # files.
+        if '-reference' in sys.argv:
+            idx_ref_file = sys.argv.index('-reference') + 1
+        else:
+            idx_ref_file = sys.argv.index('-r') + 1
+        cmd = sys.argv
+
+        simu_list = []
+        for i, d in enumerate(data):
+            f = files[i]
+            cmd[idx_ref_file] = f
+            simu_list.append(' '.join(cmd))
+
+        with Pool(processes=args.num_proc) as p:
+            total_simus = len(simu_list)
+            with tqdm(total=total_simus, desc='Running multiple simulations') as pbar:
+                for i, _ in enumerate(p.imap_unordered(run_process, simu_list)):
+                    pbar.update()
