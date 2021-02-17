Correct numerical issues with average distributions

find/plots/correlation/position_correlation.py
@@ -9,7 +9,6 @@ from find.plots.common import *
 
 
 def compute_correlation(data, tcor, ntcor, dtcor, ntcorsup, args):
-    cor_avg = np.zeros(shape=(ntcorsup, 1))
     cor_l = np.zeros(shape=(ntcorsup, 1))
     cor_f = np.zeros(shape=(ntcorsup, 1))
     ndata = np.ones(shape=(ntcorsup, 1))
@@ -20,29 +19,18 @@ def compute_correlation(data, tcor, ntcor, dtcor, ntcorsup, args):
         for itcor in range(ntcorsup):
             itp = it + itcor * ntcor
             if (itp < data[0].shape[0]):
+
                 cor0 = (data[0][it, 1] - data[0][itp, 1]) ** 2 + \
-                    (data[0][it, 0] -
-                        data[0][itp, 0]) ** 2
-                cor_avg[itcor] += cor0
-
-                cor0 = (data[0][it, 3] - data[0][itp, 3]) ** 2 + \
-                    (data[0][it, 2] -
-                        data[0][itp, 2]) ** 2
-                cor_avg[itcor] += cor0
-
-                cor0 = (data[1][it, 1] - data[1][itp, 1]) ** 2 + \
-                    (data[1][it, 0] -
-                        data[1][itp, 0]) ** 2
+                    (data[0][it, 0] - data[0][itp, 0]) ** 2
                 cor_l[itcor] += cor0
 
-                cor0 = (data[2][it, 1] - data[2][itp, 1]) ** 2 + \
-                    (data[2][it, 0] -
-                        data[2][itp, 0]) ** 2
+                cor0 = (data[1][it, 1] - data[1][itp, 1]) ** 2 + \
+                    (data[1][it, 0] - data[1][itp, 0]) ** 2
                 cor_f[itcor] += cor0
 
                 ndata[itcor] += 1
 
-    return (cor_avg, cor_l, cor_f), ndata
+    return (cor_l, cor_f), ndata
 
 
 def corx(data, ax, args):
@@ -71,7 +59,7 @@ def corx(data, ax, args):
         for idx in range(len(positions)):
             leadership_mat = np.array(leaders[idx])
             lpos = np.copy(positions[idx][:, :2])
-            fpos = np.copy(positions[idx][:, 2:])
+            fpos = np.copy(positions[idx][:, :2])
 
             idx_leaders_0 = np.where(leadership_mat[:, 1] == 0)
             idx_leaders_1 = np.where(leadership_mat[:, 1] == 1)
@@ -92,22 +80,20 @@ def corx(data, ax, args):
         dtcor = args.ntcor * args.timestep
         ntcorsup = int(args.tcor / dtcor)
 
-        cor_avg = np.zeros(shape=(ntcorsup, 1))
         cor_l = np.zeros(shape=(ntcorsup, 1))
         cor_f = np.zeros(shape=(ntcorsup, 1))
         ndata = np.ones(shape=(ntcorsup, 1))
 
         for i in tqdm(range(len(positions)), desc='Processing {}'.format(k)):
             c, n = compute_correlation(
-                (positions[i], leader_positions[i], follower_positions[i]), args.tcor, args.ntcor, dtcor, ntcorsup, args)
-            cor_avg += c[0]
-            cor_l += c[1]
-            cor_f += c[2]
+                (leader_positions[i], follower_positions[i]), args.tcor, args.ntcor, dtcor, ntcorsup, args)
+            cor_l += c[0]
+            cor_f += c[1]
             ndata += n
 
         time = np.array(range(ntcorsup)) * args.timestep
 
-        ts = cor_avg / (2*ndata)
+        ts = (cor_l + cor_f) / (2*ndata)
         ax = sns.lineplot(x=time.tolist(), y=ts.T.tolist()[0], ax=ax, color=next(colorcycler),
                           linestyle=next(linecycler), label=k)
         ts = cor_l / ndata

find/plots/correlation/relative_orientation_correlation.py
@@ -11,7 +11,6 @@ from scipy.stats import norm, rv_histogram
 
 
 def compute_correlation(data, tcor, ntcor, dtcor, ntcorsup, args):
-    cor_avg = np.zeros(shape=(ntcorsup, 1))
     cor_l = np.zeros(shape=(ntcorsup, 1))
     cor_f = np.zeros(shape=(ntcorsup, 1))
     ndata = np.ones(shape=(ntcorsup, 1))
@@ -22,21 +21,16 @@ def compute_correlation(data, tcor, ntcor, dtcor, ntcorsup, args):
         for itcor in range(ntcorsup):
             itp = it + itcor * ntcor
             if (itp < data[0].shape[0]):
+
                 cor0 = np.cos(data[0][it, 0] - data[0][itp, 0])
-                cor_avg[itcor] += cor0
-
-                cor0 = np.cos(data[0][it, 1] - data[0][itp, 1])
-                cor_avg[itcor] += cor0
-
-                cor0 = np.cos(data[1][it, 0] - data[1][itp, 0])
                 cor_l[itcor] += cor0
 
-                cor0 = np.cos(data[2][it, 0] - data[2][itp, 0])
+                cor0 = np.cos(data[1][it, 0] - data[1][itp, 0])
                 cor_f[itcor] += cor0
 
                 ndata[itcor] += 1
 
-    return (cor_avg, cor_l, cor_f), ndata
+    return (cor_l, cor_f), ndata
 
 
 def cortheta(data, ax, args):
@@ -82,22 +76,20 @@ def cortheta(data, ax, args):
         dtcor = args.ntcor * args.timestep
         ntcorsup = int(args.tcor / dtcor)
 
-        cor_avg = np.zeros(shape=(ntcorsup, 1))
         cor_l = np.zeros(shape=(ntcorsup, 1))
         cor_f = np.zeros(shape=(ntcorsup, 1))
         ndata = np.ones(shape=(ntcorsup, 1))
 
         for i in tqdm(range(len(relor)), desc='Processing {}'.format(k)):
             c, n = compute_correlation(
-                (relor[i], lrelor[i], frelor[i]), args.tcor, args.ntcor, dtcor, ntcorsup, args)
-            cor_avg += c[0]
-            cor_l += c[1]
-            cor_f += c[2]
+                (lrelor[i], frelor[i]), args.tcor, args.ntcor, dtcor, ntcorsup, args)
+            cor_l += c[0]
+            cor_f += c[1]
             ndata += n
 
         time = np.array(range(ntcorsup)) * args.timestep
 
-        ts = cor_avg / (2*ndata)
+        ts = (cor_l + cor_f) / (2*ndata)
         ax = sns.lineplot(x=time.tolist(), y=ts.T.tolist()[0], ax=ax, color=next(colorcycler),
                           linestyle=next(linecycler), label=k)
         ts = cor_l / ndata