00ca1e06325d63a95ad5616a0fcb44e9af8ad9de
Vaios Papaspyros <b.papaspyros@gmail.com>
2021-11-25 20:13:09 +0100

    Adapt plos plots to the new occupancy dist script

--- find/plots/plos/occupancy_grids.py
+++ find/plots/plos/occupancy_grids.py
@@ -27,20 +27,9 @@ def plot(exp_files, path, args):
                                1, 1, 1], 'wspace': 0.45}
                            )
 
-    sub_data = data.copy()
-    del sub_data['Hybrid']
-    del sub_data['Virtual']
-    ax[0] = go.occupancy_grid(sub_data, fig, ax[0], args, 0.2)
-
-    sub_data = data.copy()
-    del sub_data['Real']
-    del sub_data['Virtual']
-    ax[1] = go.occupancy_grid(sub_data, fig, ax[1], args, 0.2)
-
-    sub_data = data.copy()
-    del sub_data['Real']
-    del sub_data['Hybrid']
-    ax[2] = go.occupancy_grid(sub_data, fig, ax[2], args, 0.2)
+    ax[0] = go.occupancy_grid(data, fig, 'Real', ax[0], args, 0.2)
+    ax[1] = go.occupancy_grid(data, fig, 'Hybrid', ax[1], args, 0.2)
+    ax[2] = go.occupancy_grid(data, fig, 'Virtual', ax[2], args, 0.2)
 
     ax[0].text(-0.2, 1.07, r'$\mathbf{A}$',
                fontsize=25, transform=ax[0].transAxes)
@@ -59,3 +48,31 @@ def plot(exp_files, path, args):
     ax[2].grid(linestyle='dotted')
 
     plt.savefig(path + 'occupancy_maps.png')
+
+    fig, ax = plt.subplots(figsize=(10, 4),
+                           nrows=1, ncols=3,
+                           gridspec_kw={'width_ratios': [
+                               1, 1, 1], 'wspace': 0.45}
+                           )
+
+    ax[0] = go.occupancy_grid_dist(data, fig, 'Real', ax[0], args, 0.2)
+    ax[1] = go.occupancy_grid_dist(data, fig, 'Hybrid', ax[1], args, 0.2)
+    ax[2] = go.occupancy_grid_dist(data, fig, 'Virtual', ax[2], args, 0.2)
+
+    ax[0].text(-0.2, 1.07, r'$\mathbf{A}$',
+               fontsize=25, transform=ax[0].transAxes)
+    ax[1].text(-0.2, 1.07, r'$\mathbf{B}$',
+               fontsize=25, transform=ax[1].transAxes)
+    ax[2].text(-0.2, 1.07, r'$\mathbf{C}$',
+               fontsize=25, transform=ax[2].transAxes)
+    ax[0].set_ylabel('y (m)')
+    ax[0].set_xlabel('x (m)')
+    ax[1].set_ylabel('y (m)')
+    ax[1].set_xlabel('x (m)')
+    ax[2].set_ylabel('y (m)')
+    ax[2].set_xlabel('x (m)')
+    ax[0].grid(linestyle='dotted')
+    ax[1].grid(linestyle='dotted')
+    ax[2].grid(linestyle='dotted')
+
+    plt.savefig(path + 'occupancy_maps_pdfs.png')
